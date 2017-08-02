e2fa4e2c7716af841bb7b84c66d2b9376ac0546d
wanderbit <wanderbit123@mail.ru>
2017-08-02 17:39:26 +0300

    44

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { bindAll } from 'lodash';
-//import styles from './styles.less';
+import styles from './styles.less';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 
@@ -8,7 +8,7 @@ export default class HomePage extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className={styles.grey_back}>
                 <VisibleTodoList />
                 <AddTodo />
             </div>
